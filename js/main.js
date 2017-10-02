d63a3226a6bc6334bbbbe1bb95add16c13a145fe
Nurullahodjaeva Ruhsora <ruhsora@gmail.com>
2017-10-02 17:07:27 +0500

    new update

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var scroll = new SmoothScroll();
+
+var about = document.getElementById('about');
+var project  = document.getElementById('project');
+var contacts = document.getElementById('contacts');
